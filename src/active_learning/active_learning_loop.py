3136d38ffaefb27c158bda4af2dfc9de751b39d5
rida saghir <ridasaghir@ridas-MacBook-Pro.local>
2025-07-23 10:34:13 +0200

    predictions on labeled and unlabeled dataset together

--- src/active_learning/active_learning_loop.py
+++ src/active_learning/active_learning_loop.py
@@ -25,32 +25,40 @@ class ActiveLearningLoop:
         self.sampler = sampler
         self.communicator = communicator
 
-    def gather_full_dataset_view(self, labeled_manager, model):
-        embeddings, labels, filenames = labeled_manager.get_embeddings_and_labels()
+    def gather_full_dataset_view(self, manager, model):
+        embeddings, labels, filenames = [], [], []
 
-        # Predict on unlabeled set
+
         model.eval()
         with torch.no_grad():
-            for i in labeled_manager.unlabeled_indices:
-                embedding_tensor, _, filename = labeled_manager.unlabeled_dataset[i]
-                pred_logits, z = model(embedding_tensor.unsqueeze(0))
-                pred_label = torch.argmax(pred_logits, dim=1).item()
-                embeddings.append(z.squeeze(0).numpy()) # z is the learned embeddings from trainable model
+            # Labeled
+            for embedding_tensor, label_tensor, filename in manager.iter_labeled():
+                _, z = model(embedding_tensor.unsqueeze(0))
+                embeddings.append(z.squeeze(0).numpy())
+                labels.append(label_tensor.item())
+                filenames.append(filename)
+
+            # Unlabeled
+            for embedding_tensor, _, filename in manager.iter_unlabeled():
+                logits, z = model(embedding_tensor.unsqueeze(0))
+                pred_label = torch.argmax(logits, dim=1).item()
+                embeddings.append(z.squeeze(0).numpy())
                 labels.append(pred_label)
                 filenames.append(filename)
 
         return np.stack(embeddings), np.array(labels), filenames
 
-    def run(self, labeled_manager, model, num_iters=100):
+    def run(self, manager, model, num_iters=100):
         for i in range(num_iters):
-            x, y, _ = labeled_manager.next_batch()
+            x, y, _ = manager.next_batch()
             loss = self.trainer.train_step(x, y)
             print(f"Iteration {i}, Loss: {loss:.4f}")
-            embeddings, labels, filenames = self.gather_full_dataset_view(labeled_manager, model)
+
+            embeddings, labels, filenames = self.gather_full_dataset_view(manager, model)
             self.communicator.maybe_send(i, embeddings, labels, filenames)
 
             if i % 10 == 0:
-                unlabeled_loader = labeled_manager.get_unlabeled_loader()
+                unlabeled_loader = manager.get_unlabeled_loader()
                 new_ids = self.sampler.select(unlabeled_loader, model, n=5)
-                labeled_manager.add_from_unlabeled(new_ids)
+                manager.add_from_unlabeled(new_ids)
 
