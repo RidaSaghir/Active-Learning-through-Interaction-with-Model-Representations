ea801d57adea8f6bd59e312256d9f259e9032d5d
rida saghir <ridasaghir@ridas-MBP.fritz.box>
2025-07-28 11:22:13 +0200

    new batch for every iteration

--- src/dataset/labeled_manager.py
+++ src/dataset/labeled_manager.py
@@ -1,33 +1,49 @@
 from torch.utils.data import DataLoader, Subset
 import torch
 import numpy as np
+
 class LabeledSetManager:
-    def __init__(self, labeled_dataset, unlabeled_dataset, batch_size=16):
-        self.labeled_dataset = labeled_dataset
-        self.unlabeled_dataset = unlabeled_dataset
+    def __init__(self, full_dataset, labeled_indices, unlabeled_indices, batch_size=16):
+        self.dataset = full_dataset
+        self.labeled_indices = labeled_indices
+        self.unlabeled_indices = unlabeled_indices
         self.batch_size = batch_size
-        self.labeled_indices = list(range(len(labeled_dataset)))
-        self.unlabeled_indices = list(range(len(unlabeled_dataset)))
+        self._init_labeled_loader()
+
+    def _init_labeled_loader(self):
+        self.labeled_loader = DataLoader(
+            Subset(self.dataset, self.labeled_indices),
+            batch_size=self.batch_size,
+            shuffle=True
+        )
+        self.labeled_iterator = iter(self.labeled_loader)
 
     def next_batch(self):
-        loader = DataLoader(Subset(self.labeled_dataset, self.labeled_indices), batch_size=self.batch_size, shuffle=True)
-        return next(iter(loader))
+        try:
+            batch = next(self.labeled_iterator)
+        except StopIteration:
+            # Restart the iterator with a fresh shuffle
+            self._init_labeled_loader()
+            batch = next(self.labeled_iterator)
+        return batch
 
     def get_unlabeled_loader(self):
-        subset = Subset(self.unlabeled_dataset, self.unlabeled_indices)
-        return DataLoader(subset, batch_size=self.batch_size)
+        """Return a DataLoader over current unlabeled data (used in uncertainty sampling)."""
+        return DataLoader(Subset(self.dataset, self.unlabeled_indices), batch_size=self.batch_size)
 
     def add_from_unlabeled(self, indices):
+        """Move given indices from unlabeled to labeled."""
         for idx in indices:
             if idx in self.unlabeled_indices:
                 self.labeled_indices.append(idx)
                 self.unlabeled_indices.remove(idx)
 
     def iter_labeled(self):
+        """Yield (embedding, label, filename, index) for labeled data."""
         for i in self.labeled_indices:
-            yield self.labeled_dataset[i]
+            yield self.dataset[i]
 
     def iter_unlabeled(self):
+        """Yield (embedding, dummy_label, filename, index) for unlabeled data."""
         for i in self.unlabeled_indices:
-            yield self.unlabeled_dataset[i]
-
+            yield self.dataset[i]
