d9e2f85db203a5bf1498dc93779e6f7286919df9
rida saghir <ridasaghir@ridas-MBP.fritz.box>
2025-07-28 20:14:32 +0200

    route for metrics and annotations

--- src/server/routes/annotations.py
+++ src/server/routes/annotations.py
@@ -1,11 +1,37 @@
+import os
+import json
 from fastapi import APIRouter
-from ..schemas import AnnotationRequest
+from ..schemas import AnnotationRequest, HumanAnnotation
 from .. import state
+from config import HUMAN_ANNOTATIONS
 
 router = APIRouter()
 
 @router.post("/annotate")
 def receive_annotation_request(payload: AnnotationRequest):
-    state.pending_annotation_filenames = payload.filenames
+    state.suggested_annotation_filenames = payload.filenames
     print(f"[FastAPI] New samples to annotate: {len(payload.filenames)}")
     return {"status": "ok"}
+
+@router.get("/annotate")
+def get_annotation_requests():
+    if state.suggested_annotation_filenames:
+        return state.suggested_annotation_filenames
+    return {"error": "No metrics received yet"}
+
+@router.post("/human_annotations")
+def receive_human_annotations(payload: HumanAnnotation):
+    if os.path.exists(HUMAN_ANNOTATIONS):
+        with open(HUMAN_ANNOTATIONS, "r") as f:
+            annotations = json.load(f)
+    else:
+        annotations = {}
+
+    for idx, label in zip(payload.indices, payload.labels):
+        annotations[str(idx)] = label
+
+    with open(HUMAN_ANNOTATIONS, "w") as f:
+        json.dump(annotations, f, indent=2)
+
+    print(f"[FastAPI] Stored {len(payload.indices)} human annotations.")
+    return {"status": "ok", "updated": len(payload.indices)}
--- src/server/routes/metrics.py
+++ src/server/routes/metrics.py
@@ -9,3 +9,9 @@ def receive_metrics(payload: MetricsPayload):
     state.latest_metrics = payload.dict()
     print(f"[FastAPI] Received metrics at iteration {payload.iteration}")
     return {"status": "ok"}
+
+@router.get("/metrics")
+def get_latest_metrics():
+    if state.latest_metrics:
+        return state.latest_metrics
+    return {"error": "No metrics received yet"}
