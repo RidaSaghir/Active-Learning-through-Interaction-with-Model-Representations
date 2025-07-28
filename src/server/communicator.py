299099f29d6b5afd692a70255b8f4a77c969178b
rida saghir <ridasaghir@ridas-MBP.fritz.box>
2025-07-28 11:17:06 +0200

    communicator sends metrics and annotation requests

--- src/server/communicator.py
+++ src/server/communicator.py
@@ -1,27 +1,58 @@
 import requests
-from config import BROADCAST_INTERVAL
+from config import BROADCAST_INTERVAL, BASE_URL
+
 
 class RestCommunicator:
-    def __init__(self, url="http://localhost:8000/embeddings", interval=BROADCAST_INTERVAL):
+    def __init__(self, url=BASE_URL, interval=BROADCAST_INTERVAL):
         self.url = url
-        # How often during iterations, the communicator sends embeddings
+        self.embeddings_url = f"{url}/embeddings"
+        self.annotation_url = f"{url}/annotate"
+        self.metrics_url = f"{url}/metrics"
+
         self.interval = interval
 
-    def maybe_send(self, iteration, embeddings, labels, filenames):
+    def maybe_send(self, iteration, embeddings, labels, filenames, label_types):
         if iteration % self.interval == 0:
-            self.send(iteration, embeddings, labels, filenames)
+            self.send(iteration, embeddings, labels, filenames, label_types)
 
-    def send(self, iteration, embeddings, labels, filenames):
+    def send(self, iteration, embeddings, labels, filenames, label_types):
         payload = {
             "iteration": iteration,
             "embedding_shape": list(embeddings.shape),
             "embeddings": embeddings.tolist(),  # convert to JSON serializable format
             "labels": labels.tolist(),
-            "filenames": filenames
+            "filenames": filenames,
+            "label_types": label_types
         }
         try:
-            response = requests.post(self.url, json=payload)
+            response = requests.post(self.embeddings_url, json=payload)
             response.raise_for_status()
             print(f"[RestCommunicator] Sent embeddings at iteration {iteration}")
         except requests.RequestException as e:
             print(f"[RestCommunicator] Error sending data: {e}")
+
+    def send_annotation_request(self, filenames, indices):
+        payload = {
+            "filenames": filenames,
+            "indices": indices
+        }
+        try:
+            response = requests.post(self.annotation_url, json=payload)
+            response.raise_for_status()
+            print(f"[RestCommunicator] Sent {len(filenames)} samples for annotation")
+        except requests.RequestException as e:
+            print(f"[RestCommunicator] Error sending annotation request: {e}")
+
+    def send_metrics(self, iteration, accuracy, loss):
+        payload = {
+            "iteration": iteration,
+            "accuracy": accuracy,
+            "loss": loss
+        }
+        try:
+            response = requests.post(self.metrics_url, json=payload)
+            response.raise_for_status()
+            print(f"[RestCommunicator] Sent metrics at iteration {iteration}")
+        except requests.RequestException as e:
+            print(f"[RestCommunicator] Error sending metrics: {e}")
+
