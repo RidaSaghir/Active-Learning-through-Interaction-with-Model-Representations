145697ef7f51d777c335f457c7792cc2f613e2b0
rida saghir <ridasaghir@ridas-MBP.fritz.box>
2025-07-28 11:14:59 +0200

    global states

---
+++ src/server/state.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+latest_embeddings = None
+latest_iteration = None
+latest_labels = None
+latest_filenames = None
+latest_label_types = None
+
+latest_metrics = {}
+pending_annotation_filenames = []
