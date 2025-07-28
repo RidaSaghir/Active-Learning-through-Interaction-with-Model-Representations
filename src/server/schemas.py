47a00ac9a9db6f7d365237d2144e9905ff455be7
rida saghir <ridasaghir@ridas-MBP.fritz.box>
2025-07-28 20:13:08 +0200

    human annotations schema

--- src/server/schemas.py
+++ src/server/schemas.py
@@ -17,3 +17,6 @@ class MetricsPayload(BaseModel):
 class AnnotationRequest(BaseModel):
     filenames: List[str]
     indices: List[int]
+class HumanAnnotation(BaseModel):
+    indices: List[int]
+    labels: List[int]
