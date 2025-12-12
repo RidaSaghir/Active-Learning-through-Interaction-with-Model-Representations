5b4270a4ab593e87c614e41b2b267c99d319f3c4
rida saghir <ridasaghir@ridas-MBP.fritz.box>
2025-12-12 13:21:55 +0100

    to calculate human accuracy

---
+++ src/utils/lapse_rate.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import os
+import json
+from config import DATA_DIR
+
+with open("../human_annotations.json") as f:
+    human_annotations = json.load(f)
+
+metadata_path = os.path.join(DATA_DIR, "UrbanSound8K.csv")
+df = pd.read_csv(metadata_path)
+
+filename_to_true_code = {
+    row["slice_file_name"]: int(row["classID"])
+    for _, row in df.iterrows()
+}
+
+correct = 0
+total = 0
+
+for entry in human_annotations.values():
+    fname = entry["filename"]
+    human_label = entry["label"]
+    true_label = filename_to_true_code.get(fname)
+
+    if true_label is not None:
+        if human_label == true_label:
+            correct += 1
+        total += 1
+
+human_accuracy = correct / total if total > 0 else None
+print(f"Human labeling accuracy: {human_accuracy:.2%}")
